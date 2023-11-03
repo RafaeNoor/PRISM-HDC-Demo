04ff40a701003db8f4173f4799dfd319cebfd172
Russel Arbore <rarbore2@illinois.edu>
2023-11-03 16:23:08 -0500

    demo scripts

---
+++ miranda_demo.sh
@@ -0,0 +1,17 @@
+HD_CLUSTERING_DIR := ~/HD-Clustering/Hetero-C++
+
+cd $HD_CLUSTERING_DIR
+make clean
+
+# CPU
+make host-cpu
+./host-cpu 2
+mv out.txt cpu-out.txt
+
+# GPU
+make host-gpu
+./host-gpu 2
+mv out.txt gpu-out.txt
+
+#FPGA
+make host-fpga
